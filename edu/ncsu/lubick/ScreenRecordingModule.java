feaea1c4fe8364f26d63dcbc8a0a4cec19194695
Kevin Lubick <kjlubick@ncsu.edu>
2013-09-23 10:08:04 -0400

    Initial commit.  The project builds and jumps right from recording to converting.

---
+++ edu/ncsu/lubick/ScreenRecordingModule.java
@@ -0,0 +1,115 @@
+package edu.ncsu.lubick;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.Date;
+import java.util.Scanner;
+
+import com.wet.wired.jsr.converter.RecordingConverter;
+import com.wet.wired.jsr.recorder.DesktopScreenRecorder;
+import com.wet.wired.jsr.recorder.ScreenRecorderListener;
+
+public class ScreenRecordingModule implements ScreenRecorderListener
+{
+
+	private static DesktopScreenRecorder recorder;
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws Exception
+	{
+		ScreenRecorderListener recorderBoss = new ScreenRecordingModule();
+
+		System.out.println(System.getenv());
+
+		File scratchDir = new File("scratch/");
+		if (!scratchDir.exists())
+		{
+			scratchDir.mkdir();
+		}
+		String fileName = "scratch/temp.cap";
+
+		File movFile = new File(scratchDir,"temp.mov");
+		if (movFile.exists())
+		{
+			movFile.delete();
+		}
+
+
+
+		File temp = new File(fileName);
+		if (temp.exists())
+		{
+			temp.delete();
+
+		}
+		temp.createNewFile();
+
+		FileOutputStream oStream = new FileOutputStream(fileName);
+		recorder = new DesktopScreenRecorder(oStream, recorderBoss);
+		recorder.startRecording();
+		System.out.println("recording for 10 seconds");
+
+		for(int i = 1;i<=10;i++)
+		{
+			System.out.println(i);
+			Thread.sleep(1000);
+		}
+
+
+		recorder.stopRecording();
+
+		String[] newArgs = new String[]{"scratch/temp.cap"}; 
+
+
+		System.out.println("Rendering");
+
+		RecordingConverter.main(newArgs);
+
+		//ProcessBuilder pb = new ProcessBuilder("java", "-jar", "screen_converter.jar", "temp.cap");
+
+		//pb.directory(new File("scratch/"));
+		//Process p = pb.start();
+		//System.out.println(new Date());
+		//inheritIO(p.getInputStream(), System.out);
+		//inheritIO(p.getErrorStream(), System.err);
+		//System.out.println("Rendering");
+		//System.out.println(p.waitFor());
+
+		//System.out.println("Has Rendered");
+	}
+
+
+
+
+	/*private static void inheritIO(final InputStream src, final PrintStream dest) {
+		new Thread(new Runnable() {
+			public void run() {
+				Scanner sc = new Scanner(src);
+				while (sc.hasNextLine()) {
+					dest.println(new Date() + sc.nextLine());
+				}
+				sc.close();
+			}
+		}).start();
+	}*/
+
+	@Override
+	public void frameRecorded(boolean fullFrame) throws IOException
+	{
+
+
+	}
+
+	@Override
+	public void recordingStopped()
+	{
+		System.err.println("Recording Stopped");
+
+	}
+
+}

0f6c8eab14c3fcf26d0b18579007e21e7288114b
Kevin Lubick <kjlubick@ncsu.edu>
2013-09-24 14:19:47 -0400

    I think I rolled back my messing with the converter.

--- com/wet/wired/jsr/converter/RecordingConverter.java
+++ com/wet/wired/jsr/converter/RecordingConverter.java
@@ -54,8 +54,6 @@ public class RecordingConverter implements ControllerListener, DataSinkListener 
 	private boolean stateTransitionOK = true;
 
 	public static void main(String[] args) {
-		
-		args = new String[]{"scratch/temp0002.cap"};
 
 		if ((args.length != 1) || !args[0].endsWith("cap")) {
 			System.out
@@ -77,7 +75,8 @@ public class RecordingConverter implements ControllerListener, DataSinkListener 
 
 	public void process(String recordingFile, String movieFile) throws Exception {
 
-		MediaLocator mediaLocator = new MediaLocator(new File(movieFile).toURI().toURL());
+		MediaLocator mediaLocator = new MediaLocator(new File(movieFile).toURI()
+				.toURL());
 		PlayerDataSource playerDataSource = new PlayerDataSource(recordingFile);
 		Processor processor = Manager.createProcessor(playerDataSource);
 		processor.addControllerListener(this);
@@ -88,7 +87,8 @@ public class RecordingConverter implements ControllerListener, DataSinkListener 
 			return;
 		}
 
-		processor.setContentDescriptor(new ContentDescriptor(FileTypeDescriptor.QUICKTIME));
+		processor.setContentDescriptor(new ContentDescriptor(
+				FileTypeDescriptor.QUICKTIME));
 
 		TrackControl trackControl[] = processor.getTrackControls();
 		Format format[] = trackControl[0].getSupportedFormats();
